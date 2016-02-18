bb22b1a12fe247ea6f4b3a208dad532739d836cc
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-02-18 12:01:12 +0100

    Basics of cycle in aurelia

--- src/welcome.ts
+++ src/welcome.ts
@@ -51,43 +51,88 @@ export class Welcome {
 }
 
 
-const interceptMethods = ['updateTarget', 'updateSource', 'callSource'];
+const aureliaPropertySetters = new Map<string, (value)=>void>()
+const aureliaPropertyObservables = new Map<string, Rx.Observable<any>>()
+const aureliaCallObservables = new Map<string, Rx.Observable<any>>()
 
-function actOnExpression (methodName: 'updateTarget' | 'updateSource' | 'callSource', update: (newValue) => void, newValueOrEvent?) {
-  // this: binding
-  // console.log(this, methodName, update)
-  
-  if (methodName == 'updateTarget') {
-    update(newValueOrEvent) // initial value from the binding
-    console.log('target was updated', arguments) // this shouldn't happen more than once!
-  } else if (methodName == 'updateSource') { //
-    let expression = this.sourceExpression.expression
-    while (!expression.name) {
-      expression = expression.left
-    }
-    const name = expression.name
-    console.log('hi there, you changed the value of', name, newValueOrEvent)
-    // setting set externally
-  } else {
-    // update(newValueOrEvent)
-    let args = this.sourceExpression.expression.args
-    let evaluatedArgs = []
-    for (let arg of args) {
-      evaluatedArgs.push(arg.evaluate(this.source, this.lookupFunctions, true))
-    }
-    console.log('call source', arguments, evaluatedArgs)
-    // console.log('call source', this, arguments)
-  }
-    //update('yo yo yo')
-  // setTimeout(() => update('ho ho ho'), 1000)
-  // return this[method]
+function invokeAureliaBindingSetter(name, value) {
+  let setter = aureliaPropertySetters.get(name)
+  if (setter)
+    setter(value)
+  else
+    console.log('error')
+}
+function getAureliaObservableForPropertyBinding(name) {
+  return aureliaPropertyObservables.get(name)
+}
+function getAureliaObservableForFunctionBinding(name) {
+  return aureliaCallObservables.get(name)
 }
 
+function set(name, value) {
+  return { name, value }
+}
+
+function aureliaDriver(props$) {
+  props$.subscribe((propArray) => {
+    propArray.forEach((prop) => {
+      invokeAureliaBindingSetter(prop.name, prop.value)
+    })
+  })
+  // query props$ 
+  // Use props$ as instructions to fill DOM elements
+  // ...
+  const AureliaSource = {
+    select: function select(selector) {
+      return getAureliaObservableForPropertyBinding(selector)
+      // returns an object with two fields: `observable`
+      // and `events()`. The former, `observable`, is the
+      // Observable of DOM elements matching the given
+      // `selector`. Function `events(eventType)` returns
+      // the Observable of `eventType` DOM events happening
+      // on the elements matched by `selector`.
+    },
+    selectCalls: function selectCalls(selector) {
+      return getAureliaObservableForFunctionBinding(selector)
+    },
+  }
+  return AureliaSource
+}
+
+function main({ AureliaSource }) {
+  return {
+    Aurelia: AureliaSource.select('someBinding').map((value) => `${value} ha!`)
+  }
+}
+// all I need is a wrapper in attached()
+// that will run:
+//    return { Aurelia: this.cycle({ AureliaSource.select(this) }) }
+// then we can:
+/*
+cycle({ AureliaSource }) {
+  return AureliaSource.property('someBinding')
+                      .map((value) => ([
+                        set(firstName, `${value} ha!`)
+                      ]))
+}
+*/
+
+const interceptMethods = ['updateTarget', 'updateSource', 'callSource'];
 const registeredBindings = []
+// const bindingEffects = new WeakMap<string, Object>()
 
 export class InterceptBindingBehavior {
   bind(binding, scope) { // , param, param...
     const context = scope.overrideContext.bindingContext // == Welcome
+    
+    const expression = binding.sourceExpression.expression
+    let firstExpression = expression.expression || expression
+    
+    let maxNesting = 10
+    while (!firstExpression.name && maxNesting--) {
+      firstExpression = firstExpression.left
+    }
+    const name = firstExpression.name
     
     if (binding['updateTarget']) {
       let method = 'updateTarget'
@@ -100,6 +145,29 @@ export class InterceptBindingBehavior {
         updateBindingValueInView(value) 
       }).bind(binding)
     }
+    
+    // let observableInformValueChange
+    // let observableThrowError
+    // let observableComplete
+    
+    binding['observable'] = Rx.Observable.create(function (observer: Observer<any>) {
+      // Yield a single value and complete
+      binding.observableInformValueChange = (value) => observer.next(value)
+      binding.observableThrowError = (err) => observer.error(err)
+      binding.observableComplete = () => observer.complete()
+      // observer.next('42')
+      // observer.error(err)
+      // observer.complete()
+      // Any cleanup logic might go here
+      return function () {
+        binding.observableInformValueChange = null
+        binding.observableThrowError = null
+        binding.observableComplete = null
+        binding['observable'] = null
+        // console.log('disposed')
+      }
+    })
+    
     if (binding['updateSource']) {
       let method = 'updateSource'
       binding[`intercepted-${method}`] = binding[method];
@@ -108,15 +176,10 @@ export class InterceptBindingBehavior {
       //
       // we seed the value as user input to the observable 
       
-      let expression = binding.sourceExpression.expression
-      while (!expression.name) {
-        expression = expression.left
-      }
-      const name = expression.name
-      
       // const updateBindingValueInViewModel = binding[method].bind(binding);
       binding[method] = (function(value) {
         console.log('you changed the value of', name, value)
+        binding.observableInformValueChange(value)
       }).bind(binding)
     }
     if (binding['callSource']) {
@@ -124,32 +187,17 @@ export class InterceptBindingBehavior {
       binding[`intercepted-${method}`] = binding[method];
       // triggers and delegates should be considered user input
       
+      const args = firstExpression.args
+      
       binding[method] = (function($event) {
-        const args = this.sourceExpression.expression.args
+        // const args = this.sourceExpression.expression.args
         let evaluatedArgs = []
         for (let arg of args) {
           evaluatedArgs.push(arg.evaluate(this.source, this.lookupFunctions, true))
         }
-        console.log('you invoked a method', event, evaluatedArgs)
+        console.log('you invoked a method', name, event, evaluatedArgs)
       }).bind(binding)
     }
-    
-    console.log(scope.overrideContext.bindingContext) // == Welcome
-    // todo: check behaviour in repeaters
-    let i = interceptMethods.length;
-    while (i--) {
-      let method = interceptMethods[i];
-      if (!binding[method]) {
-        continue;
-      }
-      binding[`intercepted-${method}`] = binding[method];
-      let update = binding[method].bind(binding);
-      binding[method] = actOnExpression.bind(binding, method, update);
-    }
-    registeredBindings.push({
-      name: '',
-      view: Welcome
-    })
   }
 
   unbind(binding, scope) {
@@ -162,10 +210,10 @@ export class InterceptBindingBehavior {
       binding[method] = binding[`intercepted-${method}`];
       binding[`intercepted-${method}`] = null;
     }
+    if (binding.observableUnsubscribe)
+      binding.observableUnsubscribe
   }
 }
-
-
 
 export class UpperValueConverter {
   toView(value) {
